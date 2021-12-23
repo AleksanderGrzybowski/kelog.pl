9a52c18628b16114709a11b2abeb3c133fea3214
Aleksander Grzybowski <kelog@orange.pl>
2021-12-23 12:24:30 +0100

    Disable Docker cache when building

--- ci.sh
+++ ci.sh
@@ -12,7 +12,7 @@ build() {
 
   git clone "https://github.com/AleksanderGrzybowski/${repo}.git"
   cd ${repo}
-  docker build -t "kelog/${image}:latest" .
+  docker build --no-cache -t "kelog/${image}:latest" .
   kubectl -n ${NAMESPACE} get pods | grep ${pod} | awk '{print $1}' | xargs kubectl -n ${NAMESPACE} delete pod
   cd ..
   echo "Building ${repo} finished."

96d69469805a60f24f38395b056032e51d8d658e
Aleksander Grzybowski <me@aleksandergrzybowski.pl>
2023-09-21 21:05:26 +0200

    Use containerd nerdctl tool for building

--- ci.sh
+++ ci.sh
@@ -4,16 +4,23 @@ set -e
 NAMESPACE="default"
 BUILD_DIR="/tmp/ci-${RANDOM}"
 
+BUILD_TOOL="sudo nerdctl" # replace with 'docker' locally
+REGISTRY_URL="registry.kelog.pl"
+
 build() {
   repo=$1
   image=$2
   pod=$3
-  echo "Building repo = ${repo}, image = ${image}, pod = ${pod}..."
+  full_image="${REGISTRY_URL}/kelog/${image}:latest" 
+  echo "Building repo = ${repo}, full_image = ${full_image}, pod = ${pod}..."
 
   git clone "https://github.com/AleksanderGrzybowski/${repo}.git"
   cd ${repo}
-  docker build --no-cache -t "registry.kelog.pl/kelog/${image}:latest" .
-  docker push "registry.kelog.pl/kelog/${image}:latest"
+
+  ${BUILD_TOOL} build --no-cache -t ${full_image} .
+  ${BUILD_TOOL} push ${full_image}
+  ${BUILD_TOOL} system prune --force
+
   kubectl -n ${NAMESPACE} get pods | grep ${pod} | awk '{print $1}' | xargs kubectl -n ${NAMESPACE} delete pod
   cd ..
   echo "Building ${repo} finished."
@@ -27,5 +34,4 @@ build aleksandergrzybowski.pl aleksandergrzybowski.pl aleksandergrzybowski
 build 2048 2048 2048
 build csmsearch csmsearch csmsearch
 
-docker system prune --force
-
+rm -rf ${BUILD_DIR}
